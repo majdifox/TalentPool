eda234fabad5a2fe0052c92e6671bba3e5d2068b
majmehdi <majmehdi20@gmail.com>
2025-03-28 15:29:05 +0000

    finxing the routes

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,22 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\OfferController;
+use App\Http\Controllers\AppController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
+Route::apiResource('offers', OfferController::class);
+
+
+Route::post('/offer/{offerID}/apply', [AppController::class, 'apply'])->middleware('auth:sanctum');
+
+Route::delete('/applications/{appID}/withdraw', [AppController::class, 'withdraw'])->middleware('auth:sanctum');
+
+Route::get('/recruiter/applications', [AppController::class, 'getApps'])->middleware('auth:sanctum');//->(recruiter)
+Route::get('/recruiter/statistics', [AppController::class, 'getRecruiterStats'])->middleware('auth:sanctum');
+Route::put('/applications/{appID}/updateStatus', [AppController::class, 'updateAppStatus'])->middleware('auth:sanctum');
