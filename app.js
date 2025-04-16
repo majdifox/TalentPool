7501212db3859a2f9919522f2274e04fbf29dd83
majmehdi <majmehdi20@gmail.com>
2025-04-16 23:50:52 +0100

    [general] Optimisation des performances

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 // Ajout du pied de page - Implémentation 2025-04-16
 
 // Création des composants réutilisables - Implémentation 2025-04-16
+
+// Optimisation des performances - Implémentation 2025-04-16
