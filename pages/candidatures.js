ae57da75f52d854d55e619c4f74070b694258405
majmehdi <majmehdi20@gmail.com>
2025-04-16 23:50:34 +0100

    [candidatures] Ajout de l'upload de lettre de motivation

--- pages/candidatures.js
+++ pages/candidatures.js
@@ -1,3 +1,5 @@
 // Création du formulaire de candidature - Implémentation 2025-04-16
 
 // Ajout de l'upload de CV - Implémentation 2025-04-16
+
+// Ajout de l'upload de lettre de motivation - Implémentation 2025-04-16
