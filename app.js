80f05c129a0e3161fb09ef13172411e1d6e7bca2
majmehdi <majmehdi20@gmail.com>
2025-04-16 23:51:05 +0100

    [general] Finalisation de la version 1.0

--- app.js
+++ app.js
@@ -37,3 +37,5 @@
 // Finalisation de la version 1.0 - Implémentation 2025-04-16
 
 // Finalisation de la version 1.0 - Implémentation 2025-04-16
+
+// Finalisation de la version 1.0 - Implémentation 2025-04-16
