f175ee5973e9e053391e487ddf86c560f318c557
majmehdi <majmehdi20@gmail.com>
2025-04-16 23:50:24 +0100

    [annonces] Implémentation de la pagination des annonces

--- pages/annonces.js
+++ pages/annonces.js
@@ -1,3 +1,5 @@
 // Création de la page de liste des annonces - Implémentation 2025-04-16
 
 // Ajout du composant d'affichage d'une annonce - Implémentation 2025-04-16
+
+// Implémentation de la pagination des annonces - Implémentation 2025-04-16
