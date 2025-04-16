21b5f5d9637d192d84c70798dff031cbc2f02928
majmehdi <majmehdi20@gmail.com>
2025-04-16 23:50:13 +0100

    [auth] Implémentation de la validation des champs de connexion

--- pages/auth.js
+++ pages/auth.js
@@ -1,3 +1,5 @@
 // Création de la page de connexion - Implémentation 2025-04-16
 
 // Ajout du formulaire de connexion - Implémentation 2025-04-16
+
+// Implémentation de la validation des champs de connexion - Implémentation 2025-04-16
