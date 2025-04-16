a03d9d99b511f0d110cbe46af6530e561ab1c743
majmehdi <majmehdi20@gmail.com>
2025-04-16 23:50:14 +0100

    [auth] Ajout du formulaire d'inscription avec sélection de rôle

--- pages/auth.js
+++ pages/auth.js
@@ -5,3 +5,5 @@
 // Implémentation de la validation des champs de connexion - Implémentation 2025-04-16
 
 // Création de la page d'inscription - Implémentation 2025-04-16
+
+// Ajout du formulaire d'inscription avec sélection de rôle - Implémentation 2025-04-16
