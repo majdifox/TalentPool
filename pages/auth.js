2e3ea9648c7508d71ce2486ff52371142a3ad63c
majmehdi <majmehdi20@gmail.com>
2025-04-16 23:50:15 +0100

    [auth] Implémentation de la validation des champs d'inscription

--- pages/auth.js
+++ pages/auth.js
@@ -7,3 +7,5 @@
 // Création de la page d'inscription - Implémentation 2025-04-16
 
 // Ajout du formulaire d'inscription avec sélection de rôle - Implémentation 2025-04-16
+
+// Implémentation de la validation des champs d'inscription - Implémentation 2025-04-16
