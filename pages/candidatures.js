d192b257a390f5c41a0414e27190e4ee36290ff5
majmehdi <majmehdi20@gmail.com>
2025-04-16 23:50:35 +0100

    [candidatures] Implémentation de la validation des candidatures

--- pages/candidatures.js
+++ pages/candidatures.js
@@ -3,3 +3,5 @@
 // Ajout de l'upload de CV - Implémentation 2025-04-16
 
 // Ajout de l'upload de lettre de motivation - Implémentation 2025-04-16
+
+// Implémentation de la validation des candidatures - Implémentation 2025-04-16
