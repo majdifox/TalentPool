71c3a4f33ac2376ef9818aff5c9af7da46e6d122
majmehdi <majmehdi20@gmail.com>
2025-04-16 23:50:54 +0100

    [general] Amélioration de la responsivité

--- app.js
+++ app.js
@@ -5,3 +5,5 @@
 // Optimisation des performances - Implémentation 2025-04-16
 
 // Correction de bugs divers - Implémentation 2025-04-16
+
+// Amélioration de la responsivité - Implémentation 2025-04-16
